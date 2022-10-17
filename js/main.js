b4d227f45064c41a2c249b0a5bb59de5f291be89
MarkyMan4 <markus.hutnik@gmail.com>
2022-10-16 22:04:00 -0500

    added line tool and undo button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d", {willReadFrequently: true});
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let mouseDown = false;
+
+// keep track of whether something is currently being drawn, used for knowing whether to add to state list on mouseup
+let isDrawing = false; 
 let prevMouseX = 0;
 let prevMouseY = 0;
 let mouseX = 0;
 let mouseY = 0;
 let tool = "pen";
+let drawingStates = [ctx.getImageData(0, 0, canvas.width, canvas.height)]; // stack representing states of canvas, can be used for undo operation
 
 window.addEventListener("resize", (event) => {
 	canvas.width = window.innerWidth;
@@ -22,6 +26,11 @@ document.addEventListener("mousedown", (event) => {
 
 document.addEventListener("mouseup", (event) => {
 	mouseDown = false;
+
+	if(isDrawing) {
+		drawingStates.push(ctx.getImageData(0, 0, canvas.width, canvas.height)); // mouseup adds to state stack
+		isDrawing = false;
+	}
 });
 
 document.addEventListener("mousemove", (event) => {
@@ -33,8 +42,8 @@ function preventDefault(e) {
 }
 
 document.addEventListener('touchstart', (event) => {
-  document.body.addEventListener('touchmove', preventDefault, { passive: false });
-  mouseDown = true;
+	document.body.addEventListener('touchmove', preventDefault, { passive: false });
+	mouseDown = true;
 	prevMouseX = event.targetTouches[0].screenX;
 	prevMouseY = event.targetTouches[0].screenY;
 	mouseX = event.targetTouches[0].screenX;
@@ -42,26 +51,43 @@ document.addEventListener('touchstart', (event) => {
 });
 
 document.addEventListener('touchend', (event) => {
-  mouseDown = false;
+	mouseDown = false;
+
+	if(isDrawing) {
+		drawingStates.push(ctx.getImageData(0, 0, canvas.width, canvas.height)); // mouseup adds to state stack
+		isDrawing = false;
+	}
 });
 
 document.addEventListener('touchmove', (event) => {
-  document.body.addEventListener('touchmove', preventDefault, { passive: false });
+	document.body.addEventListener('touchmove', preventDefault, { passive: false });
 	handleMouseMove(event.targetTouches[0].screenX, event.targetTouches[0].screenY);
 });
 
 function handleMouseMove(x, y) {
 	if(mouseDown) {
-		prevMouseX = mouseX;
-		prevMouseY = mouseY;
-		mouseX = x;
-		mouseY = y;
+		isDrawing = true;
 
 		if(tool === "pen") {
+			updateMousePos(x, y);
 			drawLine(prevMouseX, prevMouseY, mouseX, mouseY);
 		}
 		else if(tool === "eraser") {
+			updateMousePos(x, y);
 			clearCircle();
+		}
+		else if(tool === "line") {
+			mouseX = x;
+			mouseY = y;
+
+			// save what is currently drawn
+			let drawing = drawingStates[drawingStates.length - 1];
+			
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			
+			// restore what was drawn prior to starting to draw a line, then draw the line on top of it
+			ctx.putImageData(drawing, 0, 0);
+			drawLine(prevMouseX, prevMouseY, mouseX, mouseY);
 		}
 	}
 	else {
@@ -70,6 +96,13 @@ function handleMouseMove(x, y) {
 		mouseX = x;
 		mouseY = y;
 	}
+}
+
+function updateMousePos(x, y) {
+	prevMouseX = mouseX;
+	prevMouseY = mouseY;
+	mouseX = x;
+	mouseY = y;
 }
 
 function drawLine(fromX, fromY, toX, toY) {
@@ -94,6 +127,21 @@ function clearCircle() {
 
 function clearScreen() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawingStates.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+}
+
+function undo() {
+	// should always have one item on stack (initial state)
+	// clicking undo added one item, so pop it and return
+	if(drawingStates.length <= 1) {
+		return;
+	}
+
+	// pop the last item off state, then revert to the last item in the state list
+	// need to pop twice since clicking undo adds to stack
+	drawingStates.pop();
+
+	ctx.putImageData(drawingStates[drawingStates.length - 1], 0, 0);
 }
 
 function updateCanvasColor() {
